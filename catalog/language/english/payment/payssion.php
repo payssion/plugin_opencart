3f21e0fb5e7de85c6e34bf4e78331246c3305d05
Dong <dong.liu@payssion.com>
2015-06-03 11:08:36 +0800

    updated web money japan

--- catalog/language/english/payment/payssion.php
+++ catalog/language/english/payment/payssion.php
@@ -12,7 +12,7 @@ $_['text_title_santandermx']		    = 'Santander Mexico';
 $_['text_title_banktransferjp']		= '銀行振込 Bank Transfer';
 $_['text_title_konbinijp']		    = 'コンビニ Convenience Store Payment (Konbini)';
 $_['text_title_payeasyjp']		    = 'ペイジー PayEasy';
-$_['text_title_webmoneyjp']		    = 'WebMoney';
+$_['text_title_webmoneyjp']		    = 'WebMoney Japan';
 
 $_['text_response']				= '';
 $_['text_success']				= '... your payment was successfully received.';
