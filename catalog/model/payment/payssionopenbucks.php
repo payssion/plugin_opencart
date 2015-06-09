9ad6070163c6327890e4506a3af7283d2ab2a9fd
Dong <dong.liu@payssion.com>
2015-06-09 22:22:20 +0800

    fixed opebucks model

--- catalog/model/payment/payssionopenbucks.php
+++ catalog/model/payment/payssionopenbucks.php
@@ -1,6 +1,5 @@
 <?php
 
 require_once(realpath(dirname(__FILE__)) . "/payssion.php");
-class ControllerPaymentPayssionOpenbucks extends ControllerPaymentPayssion {
-	protected $pm_id = 'openbucks';
+class ModelPaymentPayssionOnecard extends ModelPaymentPayssion {
 }
