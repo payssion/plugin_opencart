5307e2e6ef4f5c8cdb50ad3c38205612ea11f86d
Dong <dong.liu@payssion.com>
2015-08-28 16:18:24 +0800

    removed mobilemoney, paylink, senheng

--- admin/language/english/payment/payssionpaylinkmy.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once(realpath(dirname(__FILE__)) . "/payssion.php");
-
-$_['heading_title']					= 'PayQuik/Paylink Malaysia (via Payssion)';
-$_['text_payssionpaylinkmy']		= '<a href="https://www.payssion.com" target="_blank"><img src="view/image/payment/paylink_my.png" alt="Payssion" title="Payssion" style="border: 1px solid #EEEEEE;" /></a>';
--- catalog/controller/payment/payssionpaylinkmy.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once(realpath(dirname(__FILE__)) . "/payssion.php");
-class ControllerPaymentPayssionPaylinkmy extends ControllerPaymentPayssion {
-	protected $pm_id = 'paylink_my';
-}
