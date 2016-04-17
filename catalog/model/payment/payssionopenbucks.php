0749e10bf57c52e6b6c8fef99eed32e8991dfe08
payssion <github@payssion.com>
2016-04-17 15:52:32 +0800

    fixed opennbucks

--- catalog/model/payment/payssionopenbucks.php
+++ catalog/model/payment/payssionopenbucks.php
@@ -1,5 +1,5 @@
 <?php
 
 require_once(realpath(dirname(__FILE__)) . "/payssion.php");
-class ModelPaymentPayssionOnecard extends ModelPaymentPayssion {
+class ModelPaymentPayssionOpenbucks extends ModelPaymentPayssion {
 }
