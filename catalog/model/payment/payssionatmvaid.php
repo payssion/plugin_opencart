356c0dde8c96da8d805be9df395dd6e188332d63
payssion <github@payssion.com>
2017-03-07 12:38:08 +0800

    fix atmvaid model not found

--- catalog/model/payment/payssionatmvaid.php
+++ catalog/model/payment/payssionatmvaid.php
@@ -1,5 +1,5 @@
 <?php
 
 require_once(realpath(dirname(__FILE__)) . "/payssion.php");
-class ModelPaymentPayssionATMVAmy extends ModelPaymentPayssion {
+class ModelPaymentPayssionATMVAid extends ModelPaymentPayssion {
 }
